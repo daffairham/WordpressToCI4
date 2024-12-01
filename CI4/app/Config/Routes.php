4309baf032f4d99d0cc6de5885eb1d3435646e0e
daffairham <dafirham@gmail.com>
2024-12-02 02:12:59 +0700

    Buat Script untuk Buat Method per file .txt
    Masih news doang tapi

--- CI4/app/Config/Routes.php
+++ CI4/app/Config/Routes.php
@@ -6,4 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/news', 'News::index');
--- CI4/converter/converter.php
+++ CI4/converter/converter.php
@@ -1,28 +1,47 @@
 <?php
 
 $txtPath = "news.txt";
+$controllerPath = "../app/Controllers/News.php";
+$routesPath = "../app/Config/Routes.php";
+$viewsPath = "../app/Views/";
 
 $txtFile = file($txtPath);
 
-
-
-/*
-IDE SCRIPT: 
-
-1. baca file .txt per baris
-2. stlh dapet pathnya, dapetin nama halamannya (pisahin rugbyindo.id sama index.html)
-3. write method per halaman ke controller (fwrite)
-4. buat folder views dengan sesuai nama halamannya (mkdir)
-5. masukkin file index.html per halaman ke dalem folder viewsnya
-6. write route baru ke Routes.php
-7. ???
-8. jadi (mereun?)
-*/
+$controllerTemplate = "<?php
+namespace App\Controllers;
+class News extends BaseController
+{";
+file_put_contents($controllerPath, $controllerTemplate);
 
 foreach ($txtFile as $baris) {
-    $normalizedPath = str_replace(['../', '\\'], ['', '/'], $baris);
-    $pathParts = explode('/', $normalizedPath);
-    $folderName = $pathParts[count($pathParts) - 2];
-    echo $folderName;
-    fopen("../$folderName", "w");
+    $filePath = str_replace(['../', '\\'], ['', '/'], $baris);
+    $splittedFilePaths = explode('/', $filePath);
+
+    $folderName = $splittedFilePaths[count($splittedFilePaths) - 2];
+
+    $viewFolderPath = $viewsPath . "/news/" . $folderName;
+    if (!is_dir($viewFolderPath)) {
+        mkdir($viewFolderPath);
+    }
+
+    $sourceHtmlPath = "D:/Kuliah/Proyek Informatika/rugbyindonesia.or.id/" . $folderName . "/" . "index.html";
+    $destination = $viewFolderPath . "/index.php";
+    if (file_exists($sourceHtmlPath)) {
+        copy($sourceHtmlPath, $destination);
+    }
+
+    $normalizedFolderName = str_replace('-', '_', $folderName);
+    $pageFunctions = "
+    public function page_$normalizedFolderName()
+    {
+        return view('news/$folderName/index');
+    }
+";
+
+    file_put_contents($controllerPath, $pageFunctions, FILE_APPEND);
+
+    $pagesRoute = "\$routes->get('/$folderName', 'News::page_$normalizedFolderName');\n";
+    file_put_contents($routesPath, $pagesRoute, FILE_APPEND);
 }
+
+file_put_contents($controllerPath, "\n}\n", FILE_APPEND);
