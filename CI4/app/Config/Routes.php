11085e4066985ac8fa91e15cdcf5e297eec1cc8e
daffairham <dafirham@gmail.com>
2024-12-02 17:03:41 +0700

    convert script to java

--- CI4/app/Config/Routes.php
+++ CI4/app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/2012-crown-jakarta-komodos-10s-this-saturday-may-5-at-bis-en', 'News::page_2012_crown_jakarta_komodos_10s_this_saturday_may_5_at_bis_en');
+$routes->get('/2012-crown-jakarta-komodos-10s-this-saturday-may-5-at-bis-id', 'News::page_2012_crown_jakarta_komodos_10s_this_saturday_may_5_at_bis_id');
+$routes->get('/2012-indonesian-rugby-charity-lunch-was-an-overwhelming-success-en', 'News::page_2012_indonesian_rugby_charity_lunch_was_an_overwhelming_success_en');
+$routes->get('/2012-indonesian-rugby-charity-lunch-was-an-overwhelming-success-id', 'News::page_2012_indonesian_rugby_charity_lunch_was_an_overwhelming_success_id');
+$routes->get('/2012-skybattle-schools-rugby-en', 'News::page_2012_skybattle_schools_rugby_en');
---
+++ IndexHTMLFinder/src/converter.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.Scanner;
+
+public class converter {
+    
+    public static void main(String[] args) throws IOException {
+        String txtPath = "../news.txt";
+        File txtFile = new File(txtPath);
+        Scanner txtReader = new Scanner(txtFile);
+        while (txtReader.hasNextLine()) {
+            String data = txtReader.nextLine();
+            String folderName = data.substring(24);
+            String folderName2 = folderName.replace("\\index.html", "");
+            String controllerTemplate = "<?php\r\n" + //
+                                "\r\n" + //
+                                "namespace App\\Controllers;\r\n" + //
+                                "\r\n" + //
+                                "class News extends BaseController\r\n" + //
+                                "{";
+            Path newsViewPath = Paths.get("../../CI4/app/Views/news/" + folderName2);
+            
+            Path sourceHtmlPath = Paths.get("D:/Kuliah/Proyek Informatika/rugbyindonesia.or.id/" + folderName2 + "/index.html");
+            Path destination = Paths.get("../../CI4/app/Views/news/" + folderName2 + "/index.php");
+            Files.move(sourceHtmlPath, destination);
+          }
+          txtReader.close();
+    }
+}
