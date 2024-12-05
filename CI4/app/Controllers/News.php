ea6cb6b3a0e44d93e8f74b30d885045e2ec10fa9
daffairham <dafirham@gmail.com>
2024-12-05 14:42:26 +0700

    progress

---
+++ CI4/app/Controllers/News.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class News extends BaseController{
+public function page_2012_crown_jakarta_komodos_10s_this_saturday_may_5_at_bis_en(){            
+     return view('news/2012-crown-jakarta-komodos-10s-this-saturday-may-5-at-bis-en\index.html/index');
+}public function page_2012_crown_jakarta_komodos_10s_this_saturday_may_5_at_bis_id(){            {
+     return view('news/2012-crown-jakarta-komodos-10s-this-saturday-may-5-at-bis-id\index.html/index');
+}public function page_2012_indonesian_rugby_charity_lunch_was_an_overwhelming_success_en(){            {
+     return view('news/2012-indonesian-rugby-charity-lunch-was-an-overwhelming-success-en\index.html/index');
+}public function page_2012_indonesian_rugby_charity_lunch_was_an_overwhelming_success_id(){            {
+     return view('news/2012-indonesian-rugby-charity-lunch-was-an-overwhelming-success-id\index.html/index');
+}public function page_2012_skybattle_schools_rugby_en(){            {
+     return view('news/2012-skybattle-schools-rugby-en\index.html/index');
+}
+}
--- IndexHTMLFinder/src/converter.java
+++ IndexHTMLFinder/src/converter.java
@@ -10,22 +10,46 @@ public class converter {
         String txtPath = "../news.txt";
         File txtFile = new File(txtPath);
         Scanner txtReader = new Scanner(txtFile);
+        Path controllerPath = Paths.get("../../CI4/app/Controllers/");
+        File controllerFile = new File("../../CI4/app/Controllers/News.php");
+        File routerFile = new File("../../CI4/app/Config/Routes.php");
         String controllerTemplate = "<?php\r\n" + //
                                 "\r\n" + //
                                 "namespace App\\Controllers;\r\n" + //
                                 "\r\n" + //
                                 "class News extends BaseController\r\n" + //
                                 "{";
+        if(!controllerFile.exists()){
+            controllerFile.createNewFile();
+        }
+        FileWriter writer = new FileWriter(controllerFile);
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+        bufferedWriter.append(controllerTemplate);
+        BufferedWriter bwRouter = null;
         while (txtReader.hasNextLine()) {
             String data = txtReader.nextLine();
             String folderName = data.substring(24);
             String folderName2 = folderName.replace("\\index.html", "");
-            Path newsViewPath = Paths.get("../../CI4/app/Views/news/" + folderName2);
+            String formattedFolderName = folderName2.replace("-", "_");
+            String functionTemplate = "public function page_" + formattedFolderName + "(){" + //
+                                "            {\r\n" + //
+                                "     return view('news/" + folderName + "/index');\r\n" + //
+                                "}";
+            bufferedWriter.append(functionTemplate);
             
+            String pagesRoutes = "$routes->get(" + "'"  + folderName2 + "'," + "'News::page_" + formattedFolderName + "');\n";
+            FileWriter routerWriter = new FileWriter(routerFile);
+            bwRouter = new BufferedWriter(routerWriter);
+            bwRouter.append(pagesRoutes);
+            
+            Path newsViewPath = Paths.get("../../CI4/app/Views/news/" + folderName2);
+            Files.createDirectory(newsViewPath);
             Path sourceHtmlPath = Paths.get("D:/Kuliah/Proyek Informatika/rugbyindonesia.or.id/" + folderName2 + "/index.html");
             Path destination = Paths.get("../../CI4/app/Views/news/" + folderName2 + "/index.php");
-            Files.move(sourceHtmlPath, destination);
+            Files.copy(sourceHtmlPath, destination);
           }
           txtReader.close();
+          bufferedWriter.close();
+          bwRouter.close();
     }
 }
