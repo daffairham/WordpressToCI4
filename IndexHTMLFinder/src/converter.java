0dc9587effd11e8fe6c5075ddea18643ad042f9e
daffairham <dafirham@gmail.com>
2024-12-05 13:50:30 +0700

    Update converter.java

--- IndexHTMLFinder/src/converter.java
+++ IndexHTMLFinder/src/converter.java
@@ -10,16 +10,16 @@ public class converter {
         String txtPath = "../news.txt";
         File txtFile = new File(txtPath);
         Scanner txtReader = new Scanner(txtFile);
-        while (txtReader.hasNextLine()) {
-            String data = txtReader.nextLine();
-            String folderName = data.substring(24);
-            String folderName2 = folderName.replace("\\index.html", "");
-            String controllerTemplate = "<?php\r\n" + //
+        String controllerTemplate = "<?php\r\n" + //
                                 "\r\n" + //
                                 "namespace App\\Controllers;\r\n" + //
                                 "\r\n" + //
                                 "class News extends BaseController\r\n" + //
                                 "{";
+        while (txtReader.hasNextLine()) {
+            String data = txtReader.nextLine();
+            String folderName = data.substring(24);
+            String folderName2 = folderName.replace("\\index.html", "");
             Path newsViewPath = Paths.get("../../CI4/app/Views/news/" + folderName2);
             
             Path sourceHtmlPath = Paths.get("D:/Kuliah/Proyek Informatika/rugbyindonesia.or.id/" + folderName2 + "/index.html");
