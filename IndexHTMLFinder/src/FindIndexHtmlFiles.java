528549715f0a65deddb2aa4b857312f3fde8b876
MuhammadAldiRivandi <aldirivandi10@gmail.com>
2024-12-15 15:29:16 +0700

    update file java

--- IndexHTMLFinder/src/FindIndexHtmlFiles.java
+++ IndexHTMLFinder/src/FindIndexHtmlFiles.java
@@ -10,31 +10,31 @@ public class FindIndexHtmlFiles {
         File directory = new File(directoryPath);
 
         if (directory.exists() && directory.isDirectory()) {
-            searchFile(directory, directoryPath);
+            searchFile(directory, directoryPath, "single-post", "news.txt");
         } else {
             System.out.println("Invalid directory");
         }
     }
 
-    public static void searchFile(File dir, String rootPath) {
+    public static void searchFile(File dir, String rootPath, String filter, String fileName) {
         File[] files = dir.listFiles();
 
         for (File file : files) {
             if (file.isDirectory()) {
-                searchFile(file, rootPath);
+                searchFile(file, rootPath, filter, fileName);
             } else if (file.isFile() && file.getName().equalsIgnoreCase("index.html")) {
-                if (filterNewsPage(file)) {
-                    saveToTXT(file, rootPath);
+                if (filterPage(file, filter)) {
+                    saveToTXT(file, rootPath, fileName);
                 }
             }
         }
     }
 
-    public static boolean filterNewsPage(File file) {
+    public static boolean filterPage(File file, String filter) {
         try {
             Document doc = Jsoup.parse(file, "UTF-8");
             Element body = doc.body();
-            return body.hasClass("single-post");
+            return body.hasClass(filter);
         } catch (IOException e) {
             System.err.println("Error reading file: " + file.getAbsolutePath());
             e.printStackTrace();
@@ -42,13 +42,16 @@ public class FindIndexHtmlFiles {
         }
     }
 
-    public static void saveToTXT(File file, String rootPath) {
-        try (PrintStream out = new PrintStream(new FileOutputStream("news.txt", true))) {
-            String relativePath = file.getAbsolutePath().replaceFirst("^.*" + rootPath.replace("../", ""), rootPath);
+    public static void saveToTXT(File file, String rootPath, String fileName) {
+        try (PrintStream out = new PrintStream(new FileOutputStream(fileName, true))) {
+            String absolutePath = file.getAbsolutePath();
+            String adjustedRootPath = new File(rootPath).getParentFile().getAbsolutePath();
+            String relativePath = absolutePath.replace(adjustedRootPath, "..");
             out.println(relativePath);
         } catch (IOException e) {
             System.err.println("Error writing to output file for: " + file.getAbsolutePath());
             e.printStackTrace();
         }
     }
+
 }
